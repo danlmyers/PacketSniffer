491e7da617070501f14decbb9597ad96db3de0a6
moird <dan@moird.com>
2012-03-20 16:28:09 -0600

    Added appropriate Licensing.

--- PacketSniffer/ErrorHandle.cs
+++ PacketSniffer/ErrorHandle.cs
@@ -1,3 +1,20 @@
+/*This file is part of Packet Sniffer
+ * Copyright (C) 2009,2010  Daniel Myers dan<at>moird.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 /// Class for Dealing specifically with Error Messages in Try Catch Finally Blocks
 /// There is one regular Constructor that takes the Exception object and displays
 /// it. The overloaded Constructor takes the Exception Object and a String and prints
